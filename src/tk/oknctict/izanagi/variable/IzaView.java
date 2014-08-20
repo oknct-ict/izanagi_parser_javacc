f5350af3faf84ee779a4bda80aff4a36f6f23462
goya813 <wasabibeef@gmail.com>
2014-08-21 01:06:44 +0900

    GUI部品の型を作っtあ

--- src/tk/oknctict/izanagi/variable/IzaView.java
+++ src/tk/oknctict/izanagi/variable/IzaView.java
@@ -1,18 +1,128 @@
 package tk.oknctict.izanagi.variable;
 
-public class IzaNone implements IzaBasic
-{
-	public int mType;
+import java.util.ArrayList;
+import tk.oknctict.izanagi.variable.*;
+import tk.oknctict.izanagi.shell.*;
 
-	public IzaNone()
+public abstract class  IzaView implements IzaBasic
+{
+	protected int mType;
+	private float mX, mY;
+	private float mWidth, mHeight;
+	private String mText;
+
+	private IzaEvent onClick;
+
+	public int getType()
 	{
-		mType = TYPE_NONE;
+		return (mType);
 	}
+
+	public void setEvent(IzaEvent event)
+	{
+		int eventType = event.getEventType();
+
+		switch (eventType){
+			case IzaEvent.EVENT_CLICK:
+				setClickEvent(event);
+				break;
+		}
+	}
+	public IzaEvent getClickEvent()
+	{
+		return (onClick);
+	}
+	public void setClickEvent(IzaEvent event)
+	{
+		onClick = new IzaEvent(event);
+	}
+
+	public float getX()
+	{
+		return (mX);
+	}
+	public void setX(float x)
+	{
+		mX = x;
+	}
+
+	public float getY()
+	{
+		return (mY);
+	}
+	public void setY(float y)
+	{
+		mY = y;
+	}
+
+	public float getWidth()
+	{
+		return (mWidth);
+	}
+	public void setWidth(float width)
+	{
+		mWidth = width;
+	}
+
+	public float getHeight()
+	{
+		return (mHeight);
+	}
+	public void setHeight(float height)
+	{
+		mHeight = height;
+	}
+
+	public String getText()
+	{
+		return (mText);
+	}
+	public void setText(String text)
+	{
+		mText = text;
+	}
+
+	public String toString()
+	{
+		return (String.format("type = %d, x = %f, y = %f, width = %f, height = %f", mType, mX, mY, mWidth, mHeight));
+	}
+
+
+	public abstract IzaBasic cast(int type);
+	public abstract IzaBasic clone();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	public void set(IzaBasic value)
 	{
 	}
-
 	public IzaBasic add(IzaBasic right)
 	{
 		System.out.println("許されない演算です");
@@ -58,7 +168,6 @@ public class IzaNone implements IzaBasic
 		System.out.println("許されない演算です");
 		return (null);
 	}
-
 	public IzaBoolean EQ(IzaBasic right)
 	{
 		System.out.println("許されない演算です");
@@ -99,94 +208,14 @@ public class IzaNone implements IzaBasic
 		System.out.println("許されない演算です");
 		return (null);
 	}
-
 	public IzaBasic minus()
 	{
 		System.out.println("許されない演算です");
 		return (null);
 	}
-
 	public IzaBasic subscript(String index)
 	{
 		System.out.println("許されない演算です");
 		return (null);
-	}
-
-	public IzaBasic cast(int type)
-	{
-		IzaBasic result;
-		switch (type){
-			case TYPE_INTEGER:
-				result = toIzaInteger();
-				break;
-			
-			case TYPE_FLOAT:
-				result = toIzaFloat();
-				break;
-
-			case TYPE_STRING:
-				result = toIzaString();
-				break;
-
-			case TYPE_ARRAY:
-				result = toIzaArray();
-				break;
-
-			case TYPE_BUTTON:
-				result = toIzaButton();
-				break;
-
-			default:
-				System.out.println("許されないキャストです");
-				return (null);
-		}
-		
-		return (result);
-	}
-
-	public IzaBasic toIzaInteger()
-	{
-		IzaInteger value = new IzaInteger();
-		return (value);
-	}
-	public IzaBasic toIzaFloat()
-	{
-		IzaFloat value = new IzaFloat();
-		return (value);
-	}
-	public IzaBasic toIzaString()
-	{
-		IzaString value = new IzaString();
-		return (value);
-	}
-	public IzaBasic toIzaBoolean()
-	{
-		IzaBoolean value = new IzaBoolean();
-		return (value);
-	}
-	public IzaBasic toIzaArray()
-	{
-		IzaArray value = new IzaArray();
-		return (value);
-	}
-	public IzaBasic toIzaButton()
-	{
-		IzaButton value = new IzaButton();
-		return (value);
-	}
-
-	public int getType()
-	{
-		return (mType);
-	}
-
-	public IzaBasic clone()
-	{
-		return (new IzaNone());
-	}
-
-	public String toString()
-	{
-		return ("None");
 	}
 }
